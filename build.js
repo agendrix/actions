5f3a6209a73b0847ebc80dce7d3feb2eab837ac5
Eric Matte <ericmatte.inbox@gmail.com>
2020-06-29 11:06:50 -0400

    Automatically build proper actions

--- build.js
+++ build.js
@@ -4,7 +4,34 @@ const yaml = require("js-yaml");
 const ncc = require("@zeit/ncc");
 const path = require("path");
 
-const actionsToBuild = ["use-ssh", "git-diff-check"];
+let diffs = undefined;
+for (let i = 0; i < process.argv.length; i++) {
+  if (process.argv[i] === "--diff") {
+    diffs = process.argv[i + 1].split("\n");
+  }
+}
+
+/** Recursively find all actions */
+function findActionsToBuild(base, files, result) {
+  files = files || fs.readdirSync(base);
+  result = result || [];
+
+  files.forEach((file) => {
+    const newBase = path.join(base, file);
+    if (fs.statSync(newBase).isDirectory()) {
+      result = findActionsToBuild(newBase, fs.readdirSync(newBase), result);
+    } else {
+      if (file.includes("action.yml") || file.includes("action.yaml")) {
+        // If diffs is defined, then only build actions in diff.
+        if (!diffs || diffs.some((diffFile) => diffFile.startsWith(base))) {
+          result.push(base);
+        }
+      }
+    }
+  });
+
+  return result;
+}
 
 async function buildAction(path) {
   const doc = yaml.safeLoad(fs.readFileSync(`${path}/action.yml`, "utf8"));
@@ -29,6 +56,8 @@ async function buildAction(path) {
 }
 
 async function build() {
+  const actionsToBuild = findActionsToBuild(".");
+
   for (const actionPath of actionsToBuild) {
     console.log(`Action ${actionPath}`);
     await buildAction(path.join(__dirname, `./${actionPath}`));
