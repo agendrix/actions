9e270b2e71bb6e91df4a434270e8e4bd3cd4b85b
Eric Matte <eric.matte@hotmail.ca>
2020-10-09 14:49:30 -0400

    Fix deprecated set-env
    More info: https://github.blog/changelog/2020-10-01-github-actions-deprecating-set-env-and-add-path-commands/

--- use-ssh/ssh.sh
+++ use-ssh/ssh.sh
@@ -3,5 +3,5 @@ set -e
 
 eval $(ssh-agent)
 echo "${SSH_KEY}" | ssh-add -
-echo "::set-env name=SSH_AUTH_SOCK::$SSH_AUTH_SOCK"
-echo "::set-env name=SSH_AGENT_PID::$SSH_AGENT_PID"
+echo "SSH_AUTH_SOCK=$SSH_AUTH_SOCK" >> $GITHUB_ENV
+echo "SSH_AGENT_PID=$SSH_AGENT_PID" >> $GITHUB_ENV
