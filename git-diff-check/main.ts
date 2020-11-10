daaa88ffd00163be8a5937c1073184f1e81279ca
Eric Matte <eric.matte@hotmail.ca>
2020-11-10 14:53:35 -0500

    Better output for git-diff-check

--- git-diff-check/main.ts
+++ git-diff-check/main.ts
@@ -33,9 +33,12 @@ async function run() {
     core.info(tests.join("\n"));
     core.endGroup();
 
+    const diffFiles = diffs.split("\n");
     for (const test of tests) {
-      if (diffs.includes(test)) {
-        return setOutput(`Found match for "${test}".`, true);
+      for (const file of diffFiles) {
+        if (file.includes(test)) {
+          return setOutput(`Found match for "${test}" with "${file}".`, true);
+        }
       }
     }
     return setOutput("No match found.", false);
