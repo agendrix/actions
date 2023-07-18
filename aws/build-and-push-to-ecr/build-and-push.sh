4e7e91227e28ccb388d876e6b0c29cacec4e963e
Louis Boudreau <l.boudreau@agendrix.com>
2023-07-18 14:50:58 -0400

    Test

--- aws/build-and-push-to-ecr/build-and-push.sh
+++ aws/build-and-push-to-ecr/build-and-push.sh
@@ -95,6 +95,7 @@ echo "::endgroup::"
 echo "::group::Creating soci index"
  docker save "$tagged_registry_image" > image.tar
  sudo ctr image import image.tar
+ sudo ctr image ls
  sudo ctr images tag image:latest "$tagged_registry_image"
  sudo soci create "$tagged_registry_image"
  sudo soci push "$tagged_registry_image"
