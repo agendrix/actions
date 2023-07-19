0a0006b1296c34a69aaff619f2f2a5708ed01500
Louis Boudreau <l.boudreau@agendrix.com>
2023-07-19 12:09:28 -0400

    Image convert typo

--- aws/build-and-push-to-ecr/build-and-push.sh
+++ aws/build-and-push-to-ecr/build-and-push.sh
@@ -101,7 +101,7 @@ if [ "$INPUT_CREATE_SOCI_INDEX" = "true" ]; then
   echo "::group::Creating soci index"
     aws ecr get-login-password | sudo nerdctl login --username AWS --password-stdin "$INPUT_ECR_REGISTRY"
     sudo nerdctl load --input image.tar
-    sudo nerdctl convert --oci "$tagged_registry_image" "$tagged_registry_image"
+    sudo nerdctl image convert --oci "$tagged_registry_image" "$tagged_registry_image"
     sudo soci create "$tagged_registry_image"
     PASSWORD=$(aws ecr get-login-password)
     sudo soci push --user AWS:$PASSWORD "$tagged_registry_image"
