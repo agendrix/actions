449aff13dd6c27c7c520763e30b4c4bbbb37199a
Louis Boudreau <l.boudreau@agendrix.com>
2023-07-19 14:34:39 -0400

    Migrate to nerdctl

--- aws/build-and-push-to-ecr/build-and-push.sh
+++ aws/build-and-push-to-ecr/build-and-push.sh
@@ -15,9 +15,14 @@ setup_soci() {
   wget https://github.com/awslabs/soci-snapshotter/releases/download/v0.3.0/soci-snapshotter-0.3.0-linux-amd64.tar.gz
   sudo tar -C /usr/local/bin -xvf soci-snapshotter-0.3.0-linux-amd64.tar.gz
   echo "::endgroup::"
+}
 
+setup_nerdctl() {
+  echo "::group::Setting up nerdctl"
   wget "https://github.com/containerd/nerdctl/releases/download/v1.4.0/nerdctl-1.4.0-linux-amd64.tar.gz"
   sudo tar -C /usr/local/bin -xvf nerdctl-1.4.0-linux-amd64.tar.gz
+  aws ecr get-login-password | sudo nerdctl login --username AWS --password-stdin "$INPUT_ECR_REGISTRY"
+  echo "::group::Setting up nerdctl" 
 }
 
 get_tag_version_from_latest() {
@@ -71,7 +76,7 @@ if [ "$latest_tag_available" = "true" ]; then
   docker buildx build \
     --cache-to type=inline \
     --cache-from type=registry,ref="$latest_registry_image" \
-    --output type=docker,dest=image.tar,name="$tagged_registry_image" \
+    --output type=oci,dest=image.tar,name="$tagged_registry_image" \
     $INPUT_ARGS -f "$file" \
     "$INPUT_PATH";
   echo "::endgroup::"
@@ -87,10 +92,10 @@ else
 fi
 
 echo "::group::Pushing new image to ECR"
-docker load --input image.tar
-docker tag "$tagged_registry_image" "$latest_registry_image"
-docker tag "$tagged_registry_image" "$INPUT_IMAGE:latest"
-docker push "$tagged_registry_image"
+sudo nerdctl load --input image.tar
+sudo nerdctl image tag "$tagged_registry_image" "$latest_registry_image"
+sudo nerdctl image tag "$tagged_registry_image" "$INPUT_IMAGE:latest"
+sudo nerdctl push "$tagged_registry_image"
 if [ "$INPUT_SKIP_LATEST_TAG_PUSH" != "true" ]; then
   docker push "$latest_registry_image"
 fi
@@ -99,12 +104,8 @@ echo "::endgroup::"
 if [ "$INPUT_CREATE_SOCI_INDEX" = "true" ]; then
   setup_soci
   echo "::group::Creating soci index"
-    aws ecr get-login-password | sudo nerdctl login --username AWS --password-stdin "$INPUT_ECR_REGISTRY"
-    sudo nerdctl load --input image.tar
-    sudo nerdctl image convert --oci "$tagged_registry_image" "$tagged_registry_image-oci"
-    sudo soci create "$tagged_registry_image-oci"
-    PASSWORD=$(aws ecr get-login-password)
-    sudo soci push --user AWS:$PASSWORD "$tagged_registry_image-oci"
+    sudo soci create "$tagged_registry_image"
+    PASSWORD=$(aws ecr get-login-password); sudo soci push --user AWS:$PASSWORD "$tagged_registry_image"
   echo "::endgroup::"
 fi
 
